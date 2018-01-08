e6f61a3b6e3b716e598c7b3de7a8b97a57d703b8
effcamp <fcf.camposx@gmail.com>
2018-01-08 15:37:57 -0200

    Finished refactorind AddLink and Header

--- imports/components/AddLink.js
+++ imports/components/AddLink.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 
 class AddLink extends React.Component {
   onSubmit = (e) => {
--- imports/components/Link.js
+++ imports/components/Link.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { Links } from '../api/links';
 import LinksList from './LinksList';
 import AddLink from './AddLink';
 import Header from './Header';
