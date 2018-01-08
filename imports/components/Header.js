6099c2f38797e4e8575346ee1b2b4c1e13f8ca6f
effcamp <fcf.camposx@gmail.com>
2018-01-08 16:01:35 -0200

    More refactoring

--- imports/components/Header.js
+++ imports/components/Header.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import PropTypes from 'prop-types';
 
-const Header = (props) => {
-  onLogout = () => {
-    Accounts.logout();
-  };
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      <button onClick={this.onLogout}>Logout</button>
-    </div>
-  );
-};
+const Header = (props) => (
+  <div>
+    <h1>{props.title}</h1>
+    <button onClick={() => Accounts.logout()}>Logout</button>
+  </div>
+);
 
 Header.propTypes = {
   title: PropTypes.string.isRequired
